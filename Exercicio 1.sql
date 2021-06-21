f28f87c086b721c670363df6199b61b679c4f2ff
Leandro-De-Souza <leandro.desouza606@gmail.com>
2021-06-21 20:13:00 -0300

    Atualizado

--- Exercicio 1.sql
+++ Exercicio 1.sql
@@ -1,62 +1,61 @@
-create database db_pizzaria_legal;
+create database db_generation_game_online;
+use db_generation_game_online;
 
-use db_pizzaria_legal;
-
-create table tb_categoria(
+create table tb_classe (
 id bigint auto_increment not null ,
 
-tamanho varchar(255) not null,
-sabor varchar(255) not null,
-borda varchar(255) not null,
+classe varchar(255) not null,
+item varchar(255) not null,
+elemento varchar(255) not null,
 
 primary key (id) 
 );
 
-insert into tb_categoria (tamanho,sabor,borda) values ("Grande","Salgada" , "Recheada");
-insert into tb_categoria (tamanho,sabor,borda) values ("Pequena", "Salgada" , "Sem Recheio");
-insert into tb_categoria (tamanho,sabor,borda) values ("Pequena" ,"Doce" , "Com Recheio Doce");
-insert into tb_categoria (tamanho,sabor,borda) values ("Grande","Doce" , "Recheada");
-insert into tb_categoria (tamanho,sabor,borda) values ("Grande","Mista" , "Recheada");
+insert into tb_classe(classe,item,elemento) values ("Mago" , "Cajado", "Ar");
+insert into tb_classe(classe,item,elemento) values ("Arqueiro" , "Flechas", "Ar");
+insert into tb_classe(classe,item,elemento) values ("Guerreiro" , "Espada e Escudo", "Terra");
+insert into tb_classe(classe,item,elemento) values ("Curandeiro" , "Cajado", "Água");
+insert into tb_classe(classe,item,elemento) values ("Elfo" , "Diversos itens", "Fogo");
 
+select * from tb_classe;
 
-
-select * from tb_categoria;
-
-create table tb_pizza(
-
-id_pizza bigint auto_increment not null,
+create table tb_personagens(
+id_personagem bigint auto_increment not null,
 
 nome varchar(255) not null,
-preco decimal (9,2) not null,
-ingredientes varchar(255) not null,
-tempero varchar (255) not null,
+ataque int not null,
+defesa int not null,
+dano int not null,
+habilidade_especial varchar(255) not null,
 
+FK_classeid bigint,
+primary key (id_personagem),
+foreign key (FK_classeid) references tb_classe (id)
 
-FK_categoria_id bigint ,
-primary key (id_pizza),
-foreign key (FK_categoria_id) references tb_categoria (id)
 
 );
 
+insert into tb_personagens (nome,ataque,defesa,dano,habilidade_especial,FK_classeid) values ("Azazel" , 1600,2400, 550, "Explosão em área",1);
+insert into tb_personagens (nome,ataque,defesa,dano,habilidade_especial,FK_classeid) values ("Dimtri" , 2400,1750, 800, "Tank",3);
+insert into tb_personagens (nome,ataque,defesa,dano,habilidade_especial,FK_classeid) values ("Azazel" , 1800,2100, 650, "Invisibilidade",4);
+insert into tb_personagens (nome,ataque,defesa,dano,habilidade_especial,FK_classeid) values ("Cilya" , 2000,1900, 700, "Rajada",2);
+insert into tb_personagens (nome,ataque,defesa,dano,habilidade_especial,FK_classeid) values ("Arietha" , 1300,2700, 600, "Buff e regeneração",4);
+insert into tb_personagens (nome,ataque,defesa,dano,habilidade_especial,FK_classeid) values ("Lione" , 2150,1600, 950, "Teletransporte",1);
+insert into tb_personagens (nome,ataque,defesa,dano,habilidade_especial,FK_classeid) values ("Omega" , 2500,1870, 790, "Velocidade",3);
+insert into tb_personagens (nome,ataque,defesa,dano,habilidade_especial,FK_classeid) values ("Astrah" , 2000,1850, 900, "Campo de força",2);
 
-insert into tb_pizza (nome,preco,ingredientes, tempero, FK_categoria_id) values ("Mussarela" , 25.00 , "Tomate" , "Orégano" , 1);
-insert into tb_pizza (nome,preco,ingredientes, tempero, FK_categoria_id) values ("Calabresa" , 23.00 , "Cebola" , "Cheiro-Verde" , 1);
-insert into tb_pizza (nome,preco,ingredientes, tempero, FK_categoria_id) values ("Escarola" , 30.00 , "Azeitona" , "Azeite" , 2);
-insert into tb_pizza (nome,preco,ingredientes, tempero, FK_categoria_id) values ("Chocolate" , 46.00 , "M&M" , "Beijinho" , 3);
-insert into tb_pizza (nome,preco,ingredientes, tempero, FK_categoria_id) values ("Bananinha" , 35.00 , "Chocolate Ralado" , "Leite Ninho" , 5);
-insert into tb_pizza (nome,preco,ingredientes, tempero, FK_categoria_id) values ("Portuguesa" , 32.00 , "Ovo" , "Orégano" , 1);
-insert into tb_pizza (nome,preco,ingredientes, tempero, FK_categoria_id) values ("Brócolis" , 34.00 , "Palmito" , "Vinagre" , 1);
-insert into tb_pizza (nome,preco,ingredientes, tempero, FK_categoria_id) values ("Beirute Especial" , 65.00 , "Tomate, Cebola, Batata, Salada" , "Especial MIX" , 1);
+select * from tb_personagens;
+select * from tb_personagens where ataque > 2000;
+select * from tb_personagens where defesa between 1000 and 2000;
+select * from tb_personagens where nome like "C%";
 
-select * From tb_pizza;
-select * from tb_pizza where preco  > 45;
-select * from tb_pizza where preco between 29 and 60;
-select * from tb_pizza where nome like "C%";
+select * from tb_personagens 
+inner join tb_classe on tb_classe.id = tb_personagens.FK_classeid;
 
-select  tb_pizza.nome, tb_pizza.preco , tb_pizza.ingredientes , tb_pizza.tempero , tb_pizza.FK_categoria_id
-from tb_pizza inner join tb_categoria on tb_categoria.id = tb_pizza.FK_categoria_id;
+select * from tb_personagens 
+inner join tb_classe on tb_classe.id = tb_personagens.FK_classeid  where id = 2;
 
-select  tb_pizza.nome, tb_pizza.preco , tb_pizza.ingredientes , tb_pizza.tempero , tb_pizza.FK_categoria_id
-from tb_pizza inner join tb_categoria on tb_categoria.id = tb_pizza.FK_categoria_id where id = 3;
+
+
 
 
